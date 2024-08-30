f06be65fe177325c49b2f94262d7c4cbcc2bf461
Luke Geneva <lukegeneva@gmail.com>
2024-08-30 15:20:39 -0500

    Keyboard functionality

--- public/wordle.js
+++ public/wordle.js
@@ -1,5 +1,49 @@
+let gameState = {
+  attempt: 1,
+  guess: '',
+};
+
 document.addEventListener('DOMContentLoaded', () => {
-  init();
+  const keys = document.querySelectorAll('.key');
+  keys.forEach((key) => key.addEventListener('click', () => onKeyClick(key)));
 });
 
-function init() {}
+function onKeyClick(key) {
+  const innerHTML = key.innerHTML;
+  if (innerHTML === 'ENTER') {
+    onEnterClick();
+    return;
+  }
+
+  if (innerHTML === 'BACK') {
+    onDeleteClick();
+    return;
+  }
+
+  if (gameState.guess.length === 5) return;
+
+  gameState = { ...gameState, guess: gameState.guess + key.innerHTML };
+  renderGameState();
+}
+
+function onEnterClick() {
+  console.log('ENTER');
+}
+
+function onDeleteClick() {
+  gameState = {
+    ...gameState,
+    guess: gameState.guess.slice(0, gameState.guess.length - 1),
+  };
+  renderGameState();
+}
+
+function renderGameState() {
+  const rowIndex = gameState.attempt;
+  const row = document.querySelector(`.board .row:nth-child(${rowIndex})`);
+  for (let i = 0; i < 5; i++) {
+    const char = gameState.guess[i] || '';
+    const cell = row.querySelector(`.cell:nth-child(${i + 1})`);
+    cell.innerHTML = char;
+  }
+}
